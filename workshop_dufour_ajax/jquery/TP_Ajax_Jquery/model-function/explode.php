05ff9d7183bf85a25c72e20a376405bd8c99c15b
tsudos <tsud@sfr.fr>
2019-11-17 18:58:47 +0100

    ajout de explode pour prendre la liste du cour pour la mettre dans la database/modif liste mot

---
+++ workshop_dufour_ajax/jquery/TP_Ajax_Jquery/model-function/explode.php
@@ -0,0 +1,25 @@
+<?php
+//pb avec 'forme : carré' et 'pâte à modeler' il faudrait que je fasse une condition si entre deux ' il y a des espaces alors on n'enleve pas les espaces
+if (!$fp = fopen("listeMot.txt","r"))
+ {echo "Echec de l'ouverture du fichier";}
+ else {
+    $contenu = fread($fp, 3000);
+    $arraynote1 = explode('\'', $contenu );
+    $arrayimplode = implode($arraynote1);
+    $arraynote2 = explode(',', $arrayimplode);
+    $arrayimplode2 = implode($arraynote2);
+    $arraynote3 = explode(' ', $arrayimplode2);
+
+    $length= count($arraynote3)-1;
+    $i=0;
+    while($i<=$length)
+    {
+        echo $arraynote3[$i]."<br/>";
+        $i++;
+    }
+   
+
+}
+ fclose($fp); 
+
+?>
